239cf53352975dd1d0d0ac0d52c99b1e4fe45c3b
Hirotoshi Uchida <uho02741358@gmail.com>
2026-01-01 06:43:09 +0900

    Update vite.config.js

--- web/frontend/vite.config.js
+++ web/frontend/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  root: path.resolve(__dirname, 'resources/views'),
   build: {
-    outDir: 'dist',
+    outDir: path.resolve(__dirname, '../../dist'),
+    rollupOptions: {
+      input: path.resolve(__dirname, 'resources/views/index.blade.php')
+    },
     emptyOutDir: true
   }
 })
