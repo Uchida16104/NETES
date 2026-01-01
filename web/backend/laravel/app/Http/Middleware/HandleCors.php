f7e9f1fe4c9e7aed907b38022f91efdf8d6d69d5
Hirotoshi Uchida <uho02741358@gmail.com>
2026-01-01 12:45:14 +0900

    Update HandleCors.php

--- web/backend/laravel/app/Http/Middleware/HandleCors.php
+++ web/backend/laravel/app/Http/Middleware/HandleCors.php
@@ -8,3 +8,14 @@ return [
     'max_age' => 0,
     'supports_credentials' => false,
 ];
+public function handle($request, Closure $next)
+{
+    if ($request->getMethod() === "OPTIONS") {
+        return response('', 200)
+            ->header('Access-Control-Allow-Origin', 'https://netes.vercel.app')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+
+    return $next($request);
+}
