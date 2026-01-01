6f17a2a23ecd68199c238f01643d16fe95ab07d6
Hirotoshi Uchida <uho02741358@gmail.com>
2026-01-01 12:44:42 +0900

    Update HandleCors.php

--- web/backend/laravel/app/Http/Middleware/HandleCors.php
+++ web/backend/laravel/app/Http/Middleware/HandleCors.php
@@ -2,6 +2,9 @@ return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://netes.vercel.app'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
     'supports_credentials' => false,
 ];
